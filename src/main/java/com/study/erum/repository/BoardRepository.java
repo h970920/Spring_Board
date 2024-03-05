500d8f1743d9ef45bd0c9a085ffc1408241414f2
h970920 <h970920@naver.com>
2024-03-05 18:00:17 +0900

    mybatis-config 경로에러수정

--- src/main/java/com/study/erum/repository/BoardRepository.java
+++ src/main/java/com/study/erum/repository/BoardRepository.java
@@ -1,12 +1,16 @@
 package com.study.erum.repository;
 
 import com.study.erum.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
 public class BoardRepository {
+    private final SqlSessionTemplate sql;
 
-	public int save(BoardDTO boardDTO) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+    public int save(BoardDTO boardDTO) {
+        return sql.insert("Board.save", boardDTO);
+    }
 }
--- src/main/webapp/WEB-INF/views/save.jsp
+++ src/main/webapp/WEB-INF/views/save.jsp
@@ -8,6 +8,6 @@
     <input type="text" name="boardWriter" placeholder="작성자"><br><br>
     <input type="text" name="boardPass" placeholder="비밀번호"><br><br>
     <input type="text" name="boardTitle" placeholder="제목"><br><br>
-    <textarea name="boardContents" cols="30" rows="10" placeholder="내용을 입력하세요"></textarea><br><br>
+    <textarea name="boardContents" cols="24" rows="10" placeholder="내용을 입력하세요"></textarea><br><br>
     <input type="submit" value="작성">
 </form></body></html>

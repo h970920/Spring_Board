11a36190658390941a9bdeca0f2ecb536966d221
h970920 <h970920@naver.com>
2024-03-06 14:23:46 +0900

    보드컨트롤러에 list 기능 추가

--- src/main/java/com/study/erum/BoardController.java
+++ src/main/java/com/study/erum/BoardController.java
@@ -1,13 +1,19 @@
 package com.study.erum;
 
-import com.study.erum.dto.BoardDTO;
-import com.study.erum.service.BoardService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.study.erum.dto.BoardDTO;
+import com.study.erum.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
@@ -31,4 +37,20 @@ public class BoardController {
             return "save";
         }
     }
+    
+    
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+
 }
